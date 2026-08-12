faebf4fda7f7b66c13aaed45b602226abefbffee
Eschxr <46295107+Eschxr@users.noreply.github.com>
2026-08-12 20:07:17 +0100

    Added more ops and a pytorch implementation

--- 01-micrograd/README.md
+++ 01-micrograd/README.md
@@ -2,3 +2,5 @@
 
 Lesson #1: Backpropagation, the essence of neural networks, and how to write a minimal autograd engine
 
+`main.py` contains the Value class and `torchimpl.py` contains a similar implementation of scalar backprop using PyTorch API following Karpathy's example
+
--- 01-micrograd/main.py
+++ 01-micrograd/main.py
@@ -16,17 +16,22 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)     # Value wrap safeguard
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
             self.grad += 1.0 * out.grad
             other.grad += 1.0 * out.grad
-        out._backward = _backward
+        out._backward = _backward       # pass _backward() to override empty lambda created during init of out
 
         return out
 
+    def __radd__(self, other):          # radd & rmul for right op compat (i.e: 2 + a)
+        return self + other
+
     def __mul__(self, other):
-        out = Value(self.data * other.data, (self, other), '+')
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
             self.grad += other.data * out.grad
@@ -35,6 +40,32 @@ class Value:
 
         return out
 
+    def __rmul__(self, other):
+        return self * other
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data**other, (self, ), f'**{other}')
+
+        def _backward():
+            self.grad += other * self.data**(other-1) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        return other + (-self)
+    
     def tanh(self):
         x = self.data
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
@@ -46,11 +77,21 @@ class Value:
 
         return out
 
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+
+        return out
+
     def backward(self):
 
         topo = []
         visited = set()
-        def build_topo(v):
+        def build_topo(v):              # recursively construct topological DAG...
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
@@ -59,7 +100,7 @@ class Value:
         build_topo(self)
 
         self.grad = 1.0
-        for node in reversed(topo):
+        for node in reversed(topo):     # ... & propagate through backwards
             node._backward()
 
 
