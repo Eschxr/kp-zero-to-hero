6afe02d1aaaeef069581472bbc75e2a884ba3e40
Eschxr <46295107+Eschxr@users.noreply.github.com>
2026-08-12 23:02:25 +0100

    Implemented scalar neural net modules + trained a mini mock network

--- 01-micrograd/main.py
+++ 01-micrograd/main.py
@@ -1,5 +1,6 @@
 # i'm keeping everything in main for now (will clean up later)
 import math
+import random
 
 
 class Value:
@@ -104,12 +105,88 @@ class Value:
             node._backward()
 
 
+class Neuron:
+
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        # w * x + b
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
+
+    def parameters(self):
+        return self.w + [self.b]
+
+
+class Layer:
+
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()] 
+
+
+class MLP:
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+
 if __name__ == "__main__":
-    a = Value(data=2.0, label='a')
-    b = Value(data=6.0, label='b')
-    c = b * a
-    c.backward()
-    print(c, c.grad)
-    print(a, a.grad)
-    print(b, b.grad)
+    # x = [2.0, 3.0, -1.0]
+    n = MLP(3, [4, 4, 1])
+
+    xs = [
+            [2.0, 3.0, -1.0],
+            [3.0, -1.0, 0.5],
+            [0.5, 1.0, 1.0],
+            [1.0, 1.0, -1.0],
+        ]
+    ys = [1.0, -1.0, -1.0, 1.0]
+
+    iters = 100
+    for i in range(iters):                              # a very simple manual training loop
+        print("------ITERATION " + str(i) + "------")
+        ypred = [n(x) for x in xs]
+    
+        print("Predictions: ")
+        print(ypred)
+        print("Truth: ")
+        print(ys)
+        loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+        print("Loss: ")
+        print(loss)
+        loss.backward()
+
+        for p in n.parameters():
+            p.data += -0.01 * p.grad
+        print("------------------")
+
+    ypred = [n(x) for x in xs]
+    loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+    print("------FINAL RESULTS------")
+    print("Predictions: ")
+    print(ypred)
+    print("Truth: ")
+    print(ys)
+    print("Loss: ")
+    print(loss)
+    print("------END------")
 
