6d00f8a6783f0e71d109e51cce925dc255b9911a
Eschxr <46295107+Eschxr@users.noreply.github.com>
2026-08-12 19:12:52 +0100

    Implemented basic Value class, a few ops, and backprop

--- 01-micrograd/README.md
+++ 01-micrograd/README.md
@@ -1,4 +1,4 @@
 # Micrograd
 
-Lesson #1: Backpropagation, the essence of neural networks
+Lesson #1: Backpropagation, the essence of neural networks, and how to write a minimal autograd engine
 
---
+++ 01-micrograd/main.py
@@ -0,0 +1,74 @@
+# i'm keeping everything in main for now (will clean up later)
+import math
+
+
+class Value:
+    
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+        
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+
+if __name__ == "__main__":
+    a = Value(data=2.0, label='a')
+    b = Value(data=6.0, label='b')
+    c = b * a
+    c.backward()
+    print(c, c.grad)
+    print(a, a.grad)
+    print(b, b.grad)
+
