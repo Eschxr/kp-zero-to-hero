d38580cd150dd6c0754d2b3b94c96dfe9445f89a
Eschxr <46295107+Eschxr@users.noreply.github.com>
2026-08-15 23:21:22 +0100

    02-makemore; implemented bigram counter via base python dictionary

--- 02-makemore/main.py
+++ 02-makemore/main.py
@@ -1,8 +1,16 @@
 if __name__ == "__main__":
     words = open('names.txt', 'r').read().splitlines()
-    print(words[:10])
-   
-    # view bigrams of first word
-    for ch1, ch2 in zip(words[0], words[0][1:]):
-        print(ch1, ch2)
+    # print(words[:10])
+    bigram_dict = {}                            # dictionary storing bigram frequencies
+
+    # Get bigram freqs of entire dataset, stored in a python dict
+    for word in words:
+        chars = ['<S>'] + list(word) + ['<E>']  # add special start & end chars
+        for ch1, ch2 in zip(chars, chars[1:]):
+            bigram = (ch1, ch2)
+            bigram_dict[bigram] = bigram_dict.get(bigram, 0) + 1
+            # print(ch1, ch2)
+    
+    bigram_dict = sorted(bigram_dict.items(), key = lambda kv: kv[1], reverse = True)
+    print(bigram_dict)
 
