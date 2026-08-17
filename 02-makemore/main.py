d01b954d1db16d9127b1cf6e7e997e1a3a3c32cd
Eschxr <46295107+Eschxr@users.noreply.github.com>
2026-08-17 17:47:55 +0100

    Implemented stoi character counting & bigram visualization over matplotlib on WSL2 (had to wrestle with python qt bindings for a bit)

--- 02-makemore/main.py
+++ 02-makemore/main.py
@@ -1,12 +1,26 @@
 import torch
+import matplotlib
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+
 
 if __name__ == "__main__":
     N = torch.zeros((28, 28), dtype=torch.int32)
     words = open('names.txt', 'r').read().splitlines()
     
+    stoi = {}
+    for i in range(ord('a'), ord('z')+1):
+        stoi[chr(i)] = i - ord('a')
+    stoi['<S>'] = 26
+    stoi['<E>'] = 27
+    
     for word in words:
         chs = ['<S>'] + list(word) + ['<E>']
         for ch1, ch2 in zip(chs, chs[1:]):
-            pass
-            # use a string to int lookup (ord func) to index tensor and count freqs
+            ix1, ix2 = stoi[ch1], stoi[ch2]
+            N[ix1, ix2] += 1
+
+    plt.imshow(N, cmap='viridis', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
 
