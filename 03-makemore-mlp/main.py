9d6d01744bf91b2839548a04687d82cba33af3fa
Eschxr <46295107+Eschxr@users.noreply.github.com>
2026-08-20 17:15:23 +0100

    Created MLP model with basic training loop, in line with what's been done previously

--- 03-makemore-mlp/main.py
+++ 03-makemore-mlp/main.py
@@ -1,5 +1,5 @@
 """
-WIP neural net version of the bigram language model
+MLP character-level language model
 """
 
 import torch
@@ -20,58 +20,55 @@ if __name__ == "__main__":
 
     itos = {i:s for s, i in stoi.items()}
 
-    # Construct training set
-    xs, ys = [], []
+    # Construct dataset
+    block_size = 3  # character-level context length
+    X, Y = [], []
     for word in words:
-        chs = ['.'] + list(word) + ['.']
-        for ch1, ch2 in zip(chs, chs[1:]):
-            ix1, ix2 = stoi[ch1], stoi[ch2]
-            xs.append(ix1)
-            ys.append(ix2)
-    xs = torch.tensor(xs)
-    ys = torch.tensor(ys)
-    num = xs.nelement()
-    print(f'Number of examples: {num}')
-    xenc = F.one_hot(xs, num_classes=27).float()    # One-hot encoding (int -> Tensor
-    yenc = F.one_hot(ys, num_classes=27).float()    # | Tensor[int] = 1, else = 0)
-
+        # print(word)
+        context = [0] * block_size
+        for c in word + '.':
+            ix = stoi[c]
+            X.append(context)
+            Y.append(ix)
+            # print(''.join(itos[i] for i in context), '-->', itos[ix])
+            context = context[1:] + [ix]    # Slide window right & append
+    X = torch.tensor(X)
+    Y = torch.tensor(Y)
     """
-    plt.figure(figsize=(16,16))
-    plt.imshow(xenc, cmap='Blues')
-    plt.axis('off')
-    plt.show()
+    print(X.shape, X.dtype, Y.shape, Y.dtype)
+    print(X)
+    print(Y)
     """
 
-    # Neural Net
     g = torch.Generator().manual_seed(42)
-    W = torch.randn((27, 27), generator=g, requires_grad=True)   # Randomized starting weights
 
-    # Gradient descent
-    for k in range(100):
+    # Neural net
+    C = torch.randn((27, 2), generator=g)   # Bengio et al. 2003 compressed 17000 words -> 30-d space, we shall do 27 chars -> 2-d space
+
+    # Layer 1
+    W1 = torch.randn((6, 100), generator=g)
+    b1 = torch.randn(100, generator=g)
+
+    # Layer 2
+    W2 = torch.randn((100, 27), generator=g)
+    b2 = torch.randn(27, generator=g)
+    parameters = [C, W1, b1, W2, b2]
+    # print(sum(p.nelement() for p in parameters))
+    for p in parameters:
+        p.requires_grad = True
+
+    # Training
+    for i in range(100):
         # Forward pass
-        xenc = F.one_hot(xs, num_classes=27).float()
-        logits = xenc @ W           # Log-counts produced via matmul
-        counts = logits.exp()       # Exponentiate, now positive (equal to N)
-        probs = counts / counts.sum(1, keepdim=True) # Normalized counts (THIS IS SOFTMAX BTW)
-        loss = -probs[torch.arange(num), ys].log().mean() + 0.01*(W**2).mean() # The second component regularizes W, which is equivalent to when we added 1 to N to make the distribution approach uniform
-        print(f'Loss={loss.item()}, iter={k}')
+        emb = C[X]
+        h = torch.tanh(emb.view(-1, 6) @ W1 + b1)
+        logits = h @ W2 + b2
+        loss = F.cross_entropy(logits, Y)   # Previous manual calculation replaced since this is more optimized & well-behaved
+        print(f'Loss: {loss.item()}, Interval: {i}')
 
         # Backward pass
-        W.grad = None   # Zerograd
+        for p in parameters:
+            p.grad = None
         loss.backward()
-        W.data += -50 * W.grad  # Update
-
-    # Sampling
-    for i in range(20):
-        out = ""
-        ix = 0
-        while True:
-            xenc = F.one_hot(torch.tensor([ix]), num_classes=27).float()
-            logits = xenc @ W
-            counts = logits.exp()
-            p = counts / counts.sum(1, keepdim=True)
-            ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
-            if ix == 0:
-                break
-            out += itos[ix]
-        print(out)
+        for p in parameters:
+            p.data += -0.1 * p.grad
